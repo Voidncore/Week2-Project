563a35ce4b8b83cf4ee11dd5af243481a22c9034
Angel Solano <angels31927@gmail.com>
2025-06-17 12:59:14 -0400

    Adding Top-Bar

---
+++ js/dropdown.js
@@ -0,0 +1,6 @@
+function initLanguage() {
+  const selector = document.getElementById('dropdown');
+  selector?.addEventListener('change', (e) => {
+    alert(`Language set to: ${e.target.value}`);
+  });
+}
